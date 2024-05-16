4e007e914cd8a4b34cd39697240c15966327c4f8
Slava Sidorov <sidorov-vv3@mail.ru>
2024-05-16 20:19:06 +0700

    "Updated index.html, package-lock.json, package.json, and various SCSS files with new code, added Swiper dependency, and modified existing styles and layouts."
    add section hero/scss/js
    add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,20 @@
+import 'swiper/css/bundle';
 import '@/scss/index.scss';
 import { form } from './modules/form';
 import mobileNav from './modules/mobile-nav';
 import blurHeader from './modules/blurHeader';
+import { heroSwiper } from './modules/hero-swiper';
+
+
+
+// import swiperHero from './modules/hero-swiper';
 
 const init = () => {
   form();
   mobileNav();
   blurHeader();
+  heroSwiper();
+  
 };
 
 init();
