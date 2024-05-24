2c44661d8b6539dae25f2e8d4c62344e27a99497
Slava Sidorov <sidorov-vv3@mail.ru>
2024-05-24 11:31:00 +0700

    Updated index.js and index.scss files: added new imports, removed unused imports, and rearranged import order

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 import 'swiper/css/bundle';
 import '@/scss/index.scss';
+
 import { form } from './modules/form';
 import mobileNav from './modules/mobile-nav';
 import blurHeader from './modules/blurHeader';
+
 import { heroSwiper } from './modules/hero-swiper';
 import { testimonialSwiper } from './modules/testimonial-swiper';
-
-
-
-// import swiperHero from './modules/hero-swiper';
+import toggleDarkMode from './modules/toggleDarkMode';
+import scrollUp from './modules/scrollUp';
+import scrollRevealAnimation from './modules/scrollRevealAnimation';
 
 const init = () => {
   form();
@@ -16,7 +17,10 @@ const init = () => {
   blurHeader();
   heroSwiper();
   testimonialSwiper();
-  
+  toggleDarkMode();
+  scrollUp();
+  scrollRevealAnimation();
 };
 
 init();
+
