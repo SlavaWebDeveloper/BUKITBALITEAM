8360a35301938c10e103ec99cd2b843f90e872fc
Slava Sidorov <sidorov-vv3@mail.ru>
2024-05-24 11:27:44 +0700

    add toggleDarkMode

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,23 +1,30 @@
+import toggleDarkMode from './toggleDarkMode';
+
 /*=============== SHOW MENU ===============*/
 
 function mobileNav() {
   const navMenu = document.getElementById('nav-menu');
   const navToggle = document.getElementById('nav-toggle');
   const navClose = document.getElementById('nav-close');
-  
+  const darkModeToggle = document.getElementById('darkModeToggle');
+
   /* Menu show */
   if (navToggle) {
     navToggle.addEventListener('click', () => {
       navMenu.classList.add('show-menu')
     })
   }
-  
+
   /* Menu hidden */
   if (navClose) {
     navClose.addEventListener('click', () => {
       navMenu.classList.remove('show-menu')
     })
   }
+
+
+  toggleDarkMode();
+
 }
 
 export default mobileNav;
